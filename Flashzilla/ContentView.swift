9de631ad13865ec434b949e2013e07613bd81eec
mlwelker <nutria.watts.0g@icloud.com>
2023-09-25 23:47:16 -0500

    Initial Commit

---
+++ Flashzilla/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  Flashzilla
+//
+//  Created by Michael Welker on 2023-09-25.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ Flashzilla/FlashzillaApp.swift
@@ -0,0 +1,17 @@
+//
+//  FlashzillaApp.swift
+//  Flashzilla
+//
+//  Created by Michael Welker on 2023-09-25.
+//
+
+import SwiftUI
+
+@main
+struct FlashzillaApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
