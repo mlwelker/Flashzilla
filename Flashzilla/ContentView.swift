4b70d6127e57720ef51ee783b912764db3e37c7b
mlwelker <nutria.watts.0g@icloud.com>
2023-09-26 14:05:58 -0500

    handling reduceTransparency

--- Flashzilla/ContentView.swift
+++ Flashzilla/ContentView.swift
@@ -1,26 +1,15 @@
 
 import SwiftUI
 
-func withOptionalAnimation<Result>(_ animation: Animation? = .default, _ body: () throws -> Result) rethrows -> Result {
-    if UIAccessibility.isReduceMotionEnabled {
-        return try body()
-    } else {
-        return try withAnimation(animation, body)
-    }
-}
-
 struct ContentView: View {
-    @Environment(\.accessibilityReduceMotion) var reduceMotion
-    @State private var scale = 1.0
+    @Environment(\.accessibilityReduceTransparency) var reduceTransparency
     
     var body: some View {
         Text("Hello, world")
-            .scaleEffect(scale)
-            .onTapGesture {
-                withOptionalAnimation {
-                    scale *= 1.2
-                }
-            }
+            .padding()
+            .background(reduceTransparency ? .black : .black.opacity(0.5))
+            .foregroundColor(.white)
+            .clipShape(.capsule)
     }
 }
 
