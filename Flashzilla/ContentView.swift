0935ca05d3dfad21476e38f702672697b72f8e10
mlwelker <nutria.watts.0g@icloud.com>
2023-09-26 13:12:41 -0500

    timer publisher

--- Flashzilla/ContentView.swift
+++ Flashzilla/ContentView.swift
@@ -2,16 +2,20 @@
 import SwiftUI
 
 struct ContentView: View {
+    let timer = Timer.publish(every: 1, tolerance: 0.5, on: .main, in: .common).autoconnect()
+    @State private var counter = 0
+    
     var body: some View {
-        VStack {
-            Text("Hello")
-            Spacer().frame(height: 100)
-            Text("world")
-        }
-        .contentShape(Rectangle()) // makes entire VStack tap-able, incl. Spacer blank area
-        .onTapGesture {
-            print("VStack tapped")
-        }
+        Text("Hello")
+            .onReceive(timer) { time in
+                if counter == 5 {
+                    timer.upstream.connect().cancel()
+                } else {
+                    print("The time is now \(time)")
+                }
+                
+                counter += 1
+            }
     }
 }
 
