c9b214fb7e8342f2cde29bb7b2546bd5523cc027
mlwelker <nutria.watts.0g@icloud.com>
2023-09-26 00:18:04 -0500

    rotation gesture

--- Flashzilla/ContentView.swift
+++ Flashzilla/ContentView.swift
@@ -2,20 +2,20 @@
 import SwiftUI
 
 struct ContentView: View {
-    @State private var currentAmount = 0.0
-    @State private var finalAmount = 1.0
+    @State private var currentAmount = Angle.zero
+    @State private var finalAmount = Angle.zero
     
     var body: some View {
         Text("Hello, world!")
-            .scaleEffect(finalAmount + currentAmount)
+            .rotationEffect(currentAmount + finalAmount)
             .gesture(
-                MagnificationGesture()
-                    .onChanged { amount in
-                        currentAmount = amount - 1
+                RotationGesture()
+                    .onChanged { angle in
+                        currentAmount = angle
                     }
                     .onEnded { _ in
                         finalAmount += currentAmount
-                        currentAmount = 0.0
+                        currentAmount = .zero
                     }
             )
     }
