689130c111bd262a066d46dfa6c28808d36308d4
mlwelker <nutria.watts.0g@icloud.com>
2023-09-26 18:49:56 -0500

    drag gesture for moving + removing cards

--- Flashzilla/CardView.swift
+++ Flashzilla/CardView.swift
@@ -3,8 +3,10 @@ import SwiftUI
 
 struct CardView: View {
     let card: Card
+    var removal: (() -> Void)? = nil
     
     @State private var isShowingAnswer = false
+    @State private var offset = CGSize.zero
     
     var body: some View {
         ZStack {
@@ -27,6 +29,22 @@ struct CardView: View {
             .multilineTextAlignment(.center)
         }
         .frame(width: 450, height: 250)
+        .rotationEffect(.degrees(Double(offset.width / 5)))
+        .offset(x: offset.width * 5, y: 0)
+        .opacity(2 - Double(abs(offset.width / 50)))
+        .gesture(
+            DragGesture()
+                .onChanged { dragValue in
+                    offset = dragValue.translation
+                }
+                .onEnded { _ in
+                    if abs(offset.width) > 100 {
+                        removal?()
+                    } else {
+                        offset = .zero
+                    }
+                }
+        )
         .onTapGesture {
             isShowingAnswer.toggle()
         }
