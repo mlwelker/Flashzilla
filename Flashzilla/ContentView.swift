be004eaa45a1a904967e5bf238d0b3e913d3aa71
mlwelker <nutria.watts.0g@icloud.com>
2023-09-26 12:20:21 -0500

    use contentshape to tap blank areas

--- Flashzilla/ContentView.swift
+++ Flashzilla/ContentView.swift
@@ -3,22 +3,14 @@ import SwiftUI
 
 struct ContentView: View {
     var body: some View {
-        ZStack {
-            Rectangle()
-                .fill(.blue)
-                .frame(width: 300, height: 300)
-                .onTapGesture {
-                    print("Rectangle tapped")
-                }
-            
-            Circle()
-                .fill(.orange)
-                .frame(width: 300, height: 300)
-                .contentShape(Rectangle()) // causes Rectangle area to be tap-able
-                .onTapGesture {
-                    print("Circle tapped")
-                }
-                .allowsHitTesting(false) // causes Circle to no longer catch tapping
+        VStack {
+            Text("Hello")
+            Spacer().frame(height: 100)
+            Text("world")
+        }
+        .contentShape(Rectangle()) // makes entire VStack tap-able, incl. Spacer blank area
+        .onTapGesture {
+            print("VStack tapped")
         }
     }
 }
