85126cac6d5923cc0fd039c36739ceb336322444
mlwelker <nutria.watts.0g@icloud.com>
2023-09-26 19:07:31 -0500

    color-coded card swipes + accessibility version

--- Flashzilla/ContentView.swift
+++ Flashzilla/ContentView.swift
@@ -9,6 +9,7 @@ extension View {
 }
 
 struct ContentView: View {
+    @Environment(\.accessibilityDifferentiateWithoutColor) var differentiateWithoutColor
     @State private var cards = Array<Card>(repeating: Card.example, count: 10)
     
     var body: some View {
@@ -27,6 +28,27 @@ struct ContentView: View {
                         }
                         .stacked(at: index, in: cards.count)
                     }
+                }
+            }
+            
+            if differentiateWithoutColor {
+                VStack {
+                    Spacer()
+                    
+                    HStack {
+                        Image(systemName: "xmark.circle")
+                            .padding()
+                            .background(.black.opacity(0.7))
+                            .clipShape(.circle)
+                        Spacer()
+                        Image(systemName: "checkmark.circle")
+                            .padding()
+                            .background(.black.opacity(0.7))
+                            .clipShape(.circle)
+                    }
+                    .foregroundStyle(.white)
+                    .font(.title)
+                    .padding()
                 }
             }
         }
